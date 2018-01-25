ea7fdf5ef51420ee2b823d6d485b5ed739107b1d
maticlikar <matic@likar.me>
2018-01-25 12:50:54 -0800

    Made harbor display work a lot better

--- Game.js
+++ Game.js
@@ -5,9 +5,16 @@ trader = new Trader(1, 2, 'green');
 settler = new Settler();
 joker = new Joker();
 priest = new Priest();
+priest1 = new Priest();
+priest2 = new Priest();
+priest3 = new Priest();
+priest4 = new Priest();
+priest5 = new Priest();
+priest6 = new Priest();
+priest7 = new Priest();
 
 
-harborDisplay = new HarborDisplay([captain, trader, settler, joker, priest]);
+harborDisplay = new HarborDisplay([captain, trader, settler, joker, priest, priest1, priest2, priest3, priest4, priest5, priest6, priest7]);
 
 harborDisplay.display();
 
