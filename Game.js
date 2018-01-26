a8ad8c8d8123edbef0fc50ddaf587964b0a8448d
maticlikar <matic@likar.me>
2018-01-25 17:37:04 -0800

    Made harbor diplay work better.

--- Game.js
+++ Game.js
@@ -12,9 +12,10 @@ priest4 = new Priest();
 priest5 = new Priest();
 priest6 = new Priest();
 priest7 = new Priest();
+priest8 = new Priest();
 
 
-harborDisplay = new HarborDisplay([captain, trader, settler, joker, priest, priest1, priest2, priest3, priest4, priest5, priest6, priest7]);
+harborDisplay = new HarborDisplay([captain, trader, settler, joker, priest, trader, trader, trader, trader, trader, trader, trader, trader, trader, trader, trader, trader, trader, trader]);
 
 harborDisplay.display();
 
